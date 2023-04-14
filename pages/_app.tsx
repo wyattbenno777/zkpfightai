76db96a805b3ac4b9b3cd9d1424574379a28de43
WYATT BENNO <wyattbenno@gmail.com>
2023-04-14 20:41:14 +0900

    setup tailwind.css and zkecdsa lib with button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,106 @@ By Wyatt at ETHTOKYO 2023.
 */
 
 import { useState } from "react";
+import {
+  MembershipProver,
+  MembershipVerifier,
+  Tree,
+  Poseidon,
+  defaultAddressMembershipPConfig,
+  defaultPubkeyMembershipPConfig,
+  defaultPubkeyMembershipVConfig,
+  defaultAddressMembershipVConfig
+} from "@personaelabs/spartan-ecdsa";
+import {
+  ecrecover,
+  ecsign,
+  hashPersonalMessage,
+  privateToAddress,
+  privateToPublic,
+  pubToAddress
+} from "@ethereumjs/util";
 
 export default function Home() {
 
+  const proverAddressMembership = async () => {
+      const privateKey = Buffer.from("".padStart(16, "🐱"), "utf16le");
+      const message = Buffer.from("harry potter");
+      const messageHash = hashPersonalMessage(message);
+
+      const { v, r, s } = ecsign(messageHash, privateKey);
+      const signature = `0x${r.toString("hex")}${s.toString("hex")}${v.toString(16)}`;
+
+      const hasher = new Poseidon();
+      await hasher.initWasm();
+
+      const depth = 20;
+      const addressTree = new Tree(depth, hasher);
+
+      const proverAddress = BigInt(
+        "0x" + privateToAddress(privateKey).toString("hex")
+      );
+      addressTree.insert(proverAddress);
+
+      for (const member of ["🐻", "🐶", "🐼"]) {
+        const key = privateToPublic(
+          Buffer.from("".padStart(16, member), "utf16le")
+        );
+        const address = BigInt("0x" + pubToAddress(key).toString("hex"));
+        addressTree.insert(address);
+      }
+
+      const idx = addressTree.indexOf(proverAddress);
+      const proof = addressTree.createProof(idx);
+
+      console.log("Proving...");
+      console.time("Full proving time");
+
+      const membershipProver = new MembershipProver({
+        ...defaultAddressMembershipPConfig,
+        enableProfiler: true
+      });
+
+      await membershipProver.initWasm();
+
+      const { proof: zkProof, publicInput } = await membershipProver.prove(
+        signature,
+        messageHash,
+        proof
+      );
+
+      console.timeEnd("Full proving time");
+      console.log(
+        "Raw proof size (excluding public input)",
+        zkProof.length,
+        "bytes"
+      );
+
+      console.log("Verifying...");
+      const membershipVerifier = new MembershipVerifier({
+        ...defaultAddressMembershipVConfig,
+        enableProfiler: true
+      });
+      await membershipVerifier.initWasm();
+
+      console.time("Verification time");
+      const isSuccess = await membershipVerifier.verify(zkProof, publicInput.serialize());
+      console.timeEnd("Verification time");
+
+      if (isSuccess) {
+        console.log("Successfully verified proof!");
+      } else {
+        console.log("Failed to verify proof :(");
+      }
+  };
+
   return (
     <div>
+    <button
+      onClick={proverAddressMembership}
+      className="bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-3 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+    >
+      Prove Address Membership
+    </button>
     </div>
   );
 }
